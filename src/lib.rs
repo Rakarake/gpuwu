c0b918f90c060c08ab9ce23f87cb115c729c1a08
Rakarake <rak@rakarake.xyz>
2023-08-31 00:36:59 +0200

    lib and main split

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+mod texture;
+mod camera;
+mod render;
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use render::RenderState;
+
+
+// Main program loop
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // Create render state
+    let mut state = RenderState::new(window).await;
+    // Event loop
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+                match state.render() {
+                    Ok(_) => {}
+                    // Reconfigure the surface if lost
+                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // All other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
+                }
+                // Idk about this
+                state.window().request_redraw();
+            },
+            // When new frames are wanted
+            Event::AboutToWait => {
+                state.update();
+            },
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.window().id() => if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                     => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }).unwrap();
+}
+
