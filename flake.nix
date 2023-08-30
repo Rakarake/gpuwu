9c0ae44e96a61e65838b3a136befd7dfe84be3d0
Rakarake <rak@rakarake.xyz>
2023-08-30 18:21:26 +0200

    Instance buffer

--- flake.nix
+++ flake.nix
@@ -7,7 +7,23 @@
           pkgs = nixpkgs.legacyPackages.${system};
           #runDerivation = nixpkgs.mkShell { shellHook = "cargo run"; };
         in {
-          devShells.default = import ./shell.nix { inherit pkgs; };
+          devShells.default = with pkgs; mkShell rec {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              udev alsa-lib vulkan-loader
+              # To use the x11 feature
+              xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr 
+              # To use the wayland feature
+              libxkbcommon wayland
+              # Cmake
+              cmake
+              # Fontconfig
+              fontconfig
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
           #apps.default = { type = "app"; program = "${runDerivation}/bin/???????"; };
         }
       );
