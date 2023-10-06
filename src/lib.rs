cab4a847bc382d1ec531a1540853adc7d5005a84
Rakarake <rak@rakarake.xyz>
2023-10-06 16:20:28 +0200

    Updated lockfile, added cosmic-text

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,6 @@ pub async fn run() {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
             console_log::init_with_level(log::Level::Debug).expect("Couldn't initialize logger");
-            //console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
         } else {
             env_logger::init();
         }
