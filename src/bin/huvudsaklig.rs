7b9c56e9a626c8388e30a439e1691ac403a42315
Rakarake <rak@rakarake.xyz>
2023-12-17 14:59:56 +0100

    Small text to image generator in huvudsaklig

--- src/bin/huvudsaklig.rs
+++ src/bin/huvudsaklig.rs
@@ -1,60 +1,71 @@
 use gpuwu::run;
 
+use cosmic_text::{Attrs, Buffer, Color, FontSystem, Metrics, Shaping, SwashCache};
+use std::fmt::Write;
+
 fn main() {
-    use cosmic_text::{Attrs, Color, FontSystem, SwashCache, Buffer, Metrics, Shaping};
-    
     // A FontSystem provides access to detected system fonts, create one per application
     let mut font_system = FontSystem::new();
-    
+
     // A SwashCache stores rasterized glyphs, create one per application
     let mut swash_cache = SwashCache::new();
-    
+
     // Text metrics indicate the font size and line height of a buffer
-    let metrics = Metrics::new(14.0, 20.0);
-    
+    const FONT_SIZE: f32 = 14.0;
+    const LINE_HEIGHT: f32 = FONT_SIZE * 1.2;
+    let metrics = Metrics::new(FONT_SIZE, LINE_HEIGHT);
+
     // A Buffer provides shaping and layout for a UTF-8 string, create one per text widget
     let mut buffer = Buffer::new(&mut font_system, metrics);
-    
-    // Borrow buffer together with the font system for more convenient method calls
+
     let mut buffer = buffer.borrow_with(&mut font_system);
-    
+
     // Set a size for the text buffer, in pixels
-    buffer.set_size(80.0, 25.0);
-    
+    let width = 80.0;
+    let height = f32::MAX; // The height is unbounded
+    buffer.set_size(width, height);
+
     // Attributes indicate what font to choose
     let attrs = Attrs::new();
-    
+
     // Add some text!
-    buffer.set_text("Hello in the hood Rust ksldjflksjdf lkjsdlfkj slkdjflkjsdlkfjlskdjfl;aksjdf;lkaj! 🦀\n", attrs, Shaping::Advanced);
-    
+    let text = std::env::args()
+        .nth(1)
+        .unwrap_or(" Hi, Rust! 🦀 ".to_string());
+    buffer.set_text(&text, attrs, Shaping::Advanced);
+
     // Perform shaping as desired
     buffer.shape_until_scroll();
-    
-    // Inspect the output runs
-    for run in buffer.layout_runs() {
-        for glyph in run.glyphs.iter() {
-            println!("{:#?}", glyph);
+
+    // Default text color (0xFF, 0xFF, 0xFF is white)
+    const TEXT_COLOR: Color = Color::rgb(0xFF, 0xFF, 0xFF);
+
+    // Set up the canvas
+    let width = buffer.size().0;
+    let height = LINE_HEIGHT * buffer.layout_runs().count() as f32;
+    let mut canvas = vec![vec![None; width as usize]; height as usize];
+    // Render the canvas to width x height
+    let mut imgbuf = image::ImageBuffer::new(width as u32, height as u32);
+
+    // Draw to the canvas
+    buffer.draw(&mut swash_cache, TEXT_COLOR, |x, y, w, h, color| {
+        println!("x: {:?}, y: {:?}", x, y);
+        let a = color.a();
+        if a == 0 || x < 0 || x >= width as i32 || y < 0 || y >= height as i32 || w != 1 || h != 1 {
+            // Ignore alphas of 0, or invalid x, y coordinates, or unimplemented sizes
+            return;
         }
-    }
 
-    // Create a default text color
-    let text_color = Color::rgb(0xFF, 0xFF, 0xFF);
-    
+        // Scale by alpha (mimics blending with black)
+        let scale = |c: u8| (c as i32 * a as i32 / 255).clamp(0, 255) as u8;
 
-    let imgx = 80;
-    let imgy = 25;
-
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-
-    // Draw the buffer (for performance, instead use SwashCache directly)
-    let mut pixels = [[false; 80]; 25];
-    buffer.draw(&mut swash_cache, text_color, |x, y, w, h, color| {
-        // Fill in your code here for drawing rectangles
-        //println!("Drawing: {:?}, {:?}, {:?}, {:?}, {:?}", x, y, w, h, color);
-        pixels[y as usize][x as usize] = true;
+        let r = scale(color.r());
+        let g = scale(color.g());
+        let b = scale(color.b());
+        canvas[y as usize][x as usize] = Some((r, g, b));
+        
         let pixel = imgbuf.get_pixel_mut(x as u32, y as u32);
-        *pixel = image::Rgb([244_u8, 244_u8, 244_u8]);
+        *pixel = image::Rgb([r, g, b]);
     });
     imgbuf.save("fractal.png").unwrap();
 
